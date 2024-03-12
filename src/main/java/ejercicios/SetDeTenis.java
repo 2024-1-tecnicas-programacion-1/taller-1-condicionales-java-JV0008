c71d8f14e0462e9b3c00b0dc2ecff5748959fb19
JV08 <josephd.vargaso@konradlorenz.edu.co>
2024-03-11 20:52:12 -0500

    Set de tenis
    Se resolvió el ejercicio de set de tenis

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -2,25 +2,34 @@ package ejercicios;
 
 import java.util.Scanner;
 
-/**
- *
- * @author danielsanchez
- */
 public class SetDeTenis {
-    public static String evaluar(int numVictoriasA, int numVictoriasB) {
-        // TODO: Coloca aquí el código del ejercicio 1: Set de tenis
+    public static String evaluar(int numVictoriasA, int numVictoriasB) { 
+        if (numVictoriasA < 0 || numVictoriasB < 0 || numVictoriasB > 7 || numVictoriasA > 7 || numVictoriasB == numVictoriasA && numVictoriasA == 7 || (numVictoriasA == 7 && numVictoriasB <= 4) || (numVictoriasB == 7 && numVictoriasA <= 4)) {
+            return "Inválido";
+        } else if (numVictoriasA == 7 && numVictoriasB == 6) {
+            return "Ganó A";
+        } else if (numVictoriasB == 7 && numVictoriasA == 6) {
+            return "Ganó B";
+        } else if (numVictoriasB <= 7 && numVictoriasA <= 7 && numVictoriasA - numVictoriasB >= 2 && numVictoriasA >= 6) {
+            return "Ganó A";
+        } else if (numVictoriasB <= 7 && numVictoriasA <= 7 && numVictoriasB - numVictoriasA >= 2 && numVictoriasB >= 6) {
+            return "Ganó B";
+        } else if (Math.abs(numVictoriasA - numVictoriasB) <= 1 && numVictoriasB <= 7 && numVictoriasA <=7 && numVictoriasB != 7 && numVictoriasA != 7 || (numVictoriasA < 6 && numVictoriasB <= 2) || (numVictoriasB < 6 && numVictoriasA <=2)) {
+            return "Aún no termina";
+        }
         return "";
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        
+
         System.out.print("Los juegos ganador por A:");
         int numVictoriasA = lector.nextInt();
         System.out.print("Los juegos ganador por B:");
         int numVictoriasB = lector.nextInt();
-        
+
         String respuesta = evaluar(numVictoriasA, numVictoriasB);
         System.out.println(respuesta);
     }
 }
+
--- src/test/java/test/SetDeTenisTest.java
+++ src/test/java/test/SetDeTenisTest.java
@@ -20,5 +20,53 @@ public class SetDeTenisTest {
         assertEquals(valorEsperado, valorActual);
     }
     
-    // TODO: Agrega tus otros casos de prueba aquí
+    @Test
+    public void test2() {
+        String valorEsperado = "Inválido";
+        String valorActual = SetDeTenis.evaluar(3, 7);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
+    @Test
+    public void test3() {
+        String valorEsperado = "Ganó A";
+        String valorActual = SetDeTenis.evaluar(7, 6);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
+    @Test
+    public void test4() {
+        String valorEsperado = "Aún no termina";
+        String valorActual = SetDeTenis.evaluar(5, 6);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
+    @Test
+    public void test5() {
+        String valorEsperado = "Ganó B";
+        String valorActual = SetDeTenis.evaluar(5, 7);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
+    @Test
+    public void test6() {
+        String valorEsperado = "Inválido";
+        String valorActual = SetDeTenis.evaluar(8, 6);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
+    @Test
+    public void test7() {
+        String valorEsperado = "Ganó A";
+        String valorActual = SetDeTenis.evaluar(6, 4);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
+    @Test
+    public void test8() {
+        String valorEsperado = "Aún no termina";
+        String valorActual = SetDeTenis.evaluar(2, 5);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
 }
