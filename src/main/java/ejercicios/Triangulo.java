a937441d5dc600c97d36c0cb1f8c1cfdce0575ca
JV08 <josephd.vargaso@konradlorenz.edu.co>
2024-03-11 20:50:27 -0500

    Triangulo
    Se resolvió el ejercicio de triangulo

--- src/main/java/ejercicios/Triangulo.java
+++ src/main/java/ejercicios/Triangulo.java
@@ -3,13 +3,21 @@ package ejercicios;
 import static ejercicios.SetDeTenis.evaluar;
 import java.util.Scanner;
 
-/**
- *
- * @author danielsanchez
- */
 public class Triangulo {
     public static String evaluar(double a, double b, double c) {
-        // TODO: Coloca aquí el código del ejercicio 7: Triángulos
+        
+        if (a + b < c || c + b < a || c + a < b){
+            System.out.println("No es un triangulo válido");    
+        } else if (a == b && b == c) {
+            System.out.println("El triángulo es equilátero");
+        } else if (a != b && a != c && b != c) {
+            System.out.println("El triángulo es escaleno");
+        } else {
+            System.out.println("El triángulo es isósceles");
+        }
+         
+    
+        
         return "";
     }
     
@@ -21,7 +29,6 @@ public class Triangulo {
         double b = lector.nextDouble();
         System.out.print("c:");
         double c = lector.nextDouble();
-        
         String respuesta = evaluar(a, b, c);
         System.out.println(respuesta);
     }
--- src/test/java/test/TrianguloTest.java
+++ src/test/java/test/TrianguloTest.java
@@ -8,10 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author danielsanchez
- */
+// Cómo se puede hacer un test para este ejercicio y que funcione profe?????
 public class TrianguloTest {
     @Test
     public void testNoEsUnTrianguloValido() {
@@ -19,7 +16,6 @@ public class TrianguloTest {
         String valorActual = Triangulo.evaluar(3.9, 6.0, 1.2);
         assertEquals(valorEsperado, valorActual);
     }
-    
-    // TODO: Agrega tus otros casos de prueba aquí
+
     
 }
