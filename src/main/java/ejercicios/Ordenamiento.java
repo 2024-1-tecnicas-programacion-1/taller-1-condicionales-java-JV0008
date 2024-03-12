f2d341a7eddd0a63382e47f279dddb4418cc0b2c
JV08 <josephd.vargaso@konradlorenz.edu.co>
2024-03-11 20:52:35 -0500

    Ordenamiento
    Se resolvió el ejercicio de ordenamiento

--- src/main/java/ejercicios/Ordenamiento.java
+++ src/main/java/ejercicios/Ordenamiento.java
@@ -1,19 +1,21 @@
 
 package ejercicios;
 
-import static ejercicios.SetDeTenis.evaluar;
+import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- *
- * @author danielsanchez
- */
 public class Ordenamiento {
+
     public static String evaluar(int numero1, int numero2, int numero3, int numero4) {
-        // TODO: Coloca aquí el código del ejercicio 5: Ordenamiento
-        return "";
+        int[] num = {numero1, numero2, numero3, numero4};
+        Arrays.sort(num);
+        StringBuilder sb = new StringBuilder();
+        for (int i : num) {
+            sb.append(i).append(" ");
+        }
+        return sb.toString().trim();
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Número 1:");
@@ -24,8 +26,9 @@ public class Ordenamiento {
         int numero3 = lector.nextInt();
         System.out.print("Número 4:");
         int numero4 = lector.nextInt();
-        
+
         String respuesta = evaluar(numero1, numero2, numero3, numero4);
         System.out.println(respuesta);
     }
 }
+
--- src/test/java/test/OrdenamientoTest.java
+++ src/test/java/test/OrdenamientoTest.java
@@ -19,7 +19,25 @@ public class OrdenamientoTest {
         String valorActual = Ordenamiento.evaluar(7, 0, 6, 1);
         assertEquals(valorEsperado, valorActual);
     }
+    @Test
+    public void testNo2() {
+        String valorEsperado = "-6 -1 9 20";
+        String valorActual = Ordenamiento.evaluar(9, 20, -6, -1);
+        assertEquals(valorEsperado, valorActual);
+    }
+    @Test
+    public void testNo3() {
+        String valorEsperado = "20 30 40 100";
+        String valorActual = Ordenamiento.evaluar(100, 20, 30, 40);
+        assertEquals(valorEsperado, valorActual);
+    }
+    @Test
+    public void testNo4() {
+        String valorEsperado = "-30 10 65 73";
+        String valorActual = Ordenamiento.evaluar(10, -30, 65, 73);
+        assertEquals(valorEsperado, valorActual);
+    }
     
-    // TODO: Agrega tus otros casos de prueba aquí
+
     
 }
